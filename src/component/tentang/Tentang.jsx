deaa32feacae12e5a33c773ce3aa4697714169cb
Rafisyah <m.arifin.putrasetia@gmail.com>
2023-01-12 16:46:09 +0700

    footer update

--- src/component/tentang/Tentang.jsx
+++ src/component/tentang/Tentang.jsx
@@ -4,7 +4,10 @@ import './tentang.css'
 const Tentang = () => {
   return (
     <div className='container about-container'>
-      <h1 className='about-title'>
+      <h1 className='soon'>
+      COMING SOON....
+    </h1>
+      {/* <h1 className='about-title'>
         My Biodata
       </h1>
 
@@ -12,7 +15,7 @@ const Tentang = () => {
         <ul> Personal Biodata
           <li>Name: Muhammad Rafisyah Rizkiyawan</li>
         </ul>
-      </div>
+      </div> */}
 
     </div>
   )
